dc624212b7bf30699462b12b74a50659e8225eb0
ElverTunubala <elvertunubala@gmail.com>
2024-08-14 21:29:47 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './exception.filter';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoansModule } from './loans/loans.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [LoansModule, UsersModule],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    AppService,
+  ],
+})
+export class AppModule {}
